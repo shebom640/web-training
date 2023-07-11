a31ce334de97ea4d04e7d00079ab81779be2c31b
Shebom Sarkar <sarkarshebom0@gmail.com>
2023-07-11 23:46:34 +0530

    zomato_working_1

---
+++ project_zomato/scripts/index.js
@@ -0,0 +1,12 @@
+function login() {
+    let emailAddress = document.getElementById('floatingInput').value;
+    let password = document.getElementById('floatingPassword').value;
+    console.log("Email Address: " + emailAddress);
+    console.log("Password: " + password);
+
+}
+
+function clicked() {
+    window.alert("Clicked!")
+    console.log("Card Clicked!")
+}
